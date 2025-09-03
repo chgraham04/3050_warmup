22821f9b15d1cbd7b39682ca485477dee483de97
lb26227 <lillian.ballantine@uvm.edu>
2025-09-03 15:02:07 -0400

    Remove Definition and successfully adds new data to cloud

--- admin.py
+++ admin.py
@@ -1,13 +1,19 @@
-def init_firestore_client_application_default():
-    # [START init_firestore_client_application_default]
-    import firebase_admin
-    from firebase_admin import credentials
-    from firebase_admin import firestore
+# [START init_firestore_client_application_default]
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
-    # Use the sdk credentials
-    cred = credentials.Certificate('sdk_key.json')
+# Use the sdk credentials
+cred = credentials.Certificate('../sdk_key.json') # grabs key from parent local folder
 
-    firebase_admin.initialize_app(cred)
-    db = firestore.client()
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
-    # [END init_firestore_client_application_default]
+# [END init_firestore_client_application_default]
+    
+
+
+data = {"type": "Truck", "Make": "Jeep", "Mileage": "80000"}
+
+# Add a new doc in collection 'cities' with ID 'LA'
+db.collection("Vehicle").document("LA").set(data)
