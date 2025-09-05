af113e238aa7f4e37dbfe1cb923e9b24d3f78db1
chgraham04 <143418077+chgraham04@users.noreply.github.com>
2025-09-05 15:10:18 -0400

    cli_helper.py and execute_cli.py added

---
+++ cli_helper.py
@@ -0,0 +1,8 @@
+"""
+Implement all backend logic for CLI
+Define functions to facilitate logical user interaction with all parts of CLI
+Establish connection with firestore database by scanning locally for sdk_key.json
+Reference query fields from utils.py
+Implement operators defined in utils.py
+Raise exceptions defined in utils.py
+"""
---
+++ execute_cli.py
@@ -0,0 +1,9 @@
+"""
+Organize logical navigation through CLI
+call functions defined in cli_helper.py
+"""
+def execute_cli():
+    return
+
+if __name__ == "__main__":
+    execute_cli()
