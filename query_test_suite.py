97fc674e955c7cf35a17cbc11e92983429b0c270
hannah <hdeyst@uvm.edu>
2025-09-22 15:31:16 -0400

    wrote simple test case for numeric stmts and equality

--- query_test_suite.py
+++ query_test_suite.py
@@ -1,5 +1,5 @@
 from admin import open_firestore_db
-from query_fs import run_query
+from query_fs import run_query, build_query
 """ TEST COMPOUND QUERIES"""
 # TODO: test compound queries- do all of these for both and & or
 # 2 numeric stmts
@@ -23,6 +23,12 @@ def test_numeric_stmt():
     db = open_firestore_db()
     query = "price == 6755"
     # should return a toyota highlander w/ vim 0HLRTRZR0WDUB5331
-    results = run_query(query, db)
-    print(results)
+    correct_vim = "0HLRTRZR0WDUB5331"
+    results = build_query(query, db)
+    vims = [r.id for r in results]
+    if vims != [correct_vim]:
+        raise Exception("FAILED numeric stmt test case\n"
+                        "Incorrect VIM found for price test query")
+
+    print("PASSED numeric stmt test case")
 
