b1129dfb47aa0f9f441066bd5c58e91a7ffabdac
hannah <hdeyst@uvm.edu>
2025-09-22 12:51:07 -0400

    started mapping out tests for query

---
+++ query_test_suite.py
@@ -0,0 +1,18 @@
+import execute_cli
+
+
+# TODO: test compound queries- do all of these for both and & or
+# 2 numeric stmts
+# one numeric and one string stmt (check each side)
+# 2 string query stmts
+
+# TODO: test single stmt queries
+# numeric
+# string
+
+
+query_list = [
+    "make == Toyota",
+    "model == Highlander",
+    "make == Toyota and model == Highlander",
+]
