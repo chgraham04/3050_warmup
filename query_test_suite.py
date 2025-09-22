140f0013c88057a1689cd079110791eaa27b50d3
hannah <hdeyst@uvm.edu>
2025-09-22 14:42:00 -0400

    do we have getters for vehicle attributes?

--- query_test_suite.py
+++ query_test_suite.py
@@ -1,6 +1,6 @@
-import execute_cli
-
-
+from admin import open_firestore_db
+from query_fs import run_query
+""" TEST COMPOUND QUERIES"""
 # TODO: test compound queries- do all of these for both and & or
 # 2 numeric stmts
 # one numeric and one string stmt (check each side)
@@ -9,6 +9,7 @@ import execute_cli
 # TODO: test single stmt queries
 # numeric
 # string
+# check each comparison operator
 
 
 query_list = [
@@ -16,3 +17,12 @@ query_list = [
     "model == Highlander",
     "make == Toyota and model == Highlander",
 ]
+
+""" TEST SINGLE STATEMENT QUERIES"""
+def test_numeric_stmt():
+    db = open_firestore_db()
+    query = "price == 6755"
+    # should return a toyota highlander w/ vim 0HLRTRZR0WDUB5331
+    results = run_query(query, db)
+    print(results)
+
